c595cfb1dcb0c79560b1824482adaab63c97d32b
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-11 15:01:31 +0530

    Jun 11  15:01:31

--- LRU/testcases.py
+++ LRU/testcases.py
@@ -1,9 +1,9 @@
 import unittest
 from operations import *
 
-class TestCases(unittest.TestCase) as self :
-    self.assertTrue(add(1,2), 3)
-    self.assertFalse(add(2,3), 4)
+class TestCases(unittest.TestCase) :
+    assertTrue(add(1,2), 3)
+    assertFalse(add(2,3), 4)
 
 if __name__=="__main__" :
     unittest.main()
