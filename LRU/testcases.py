17b24458b908fba419a9c6f93e66cfd4c3ad6a94
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-11 15:26:26 +0530

    Jun 11  15:26:26

--- LRU/testcases.py
+++ LRU/testcases.py
@@ -4,7 +4,7 @@ from operations import *
 class Testing(unittest.TestCase) :
     def test_add(self) :
         self.assertTrue(add(1,2), 3)
-        self.assertTrue(add(2,3), 5)
+        self.assertTrue(add(2,3), 6)
         
 if __name__=="__main__" :
     unittest.main()
