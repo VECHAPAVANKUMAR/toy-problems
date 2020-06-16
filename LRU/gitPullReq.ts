2d54a43cf13f888b62efb843ff656748a175714f
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-16 12:55:53 +0530

    Finished

---
+++ LRU/gitPullReq.ts
@@ -0,0 +1,15 @@
+const headers = {
+    "Authorization" : "Token 2f34bca63d5d6a2074053886ec97c3a1cab7944b"
+};
+fetch('https://api.github.com/repos/VECHAPAVANKUMAR/toy-problems/pulls', {
+    "method": "post",
+    "headers": headers,
+    "body": JSON.stringify({
+        "title": "New Pull Request",
+        "body": "Please pull these awesome changes in!",
+        "head": "master",
+        "base": "master",
+    })
+  })
+.then((res: { json: () => any; }) => res.json())
+.then((json: any) => console.log(json));
