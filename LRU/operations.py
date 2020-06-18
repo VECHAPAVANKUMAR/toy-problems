7f27f70d4281a8dd5f955c46b13f1f8ec144dd51
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-18 13:23:57 +0530

    Jun 18  13:23:56

--- LRU/operations.py
+++ LRU/operations.py
@@ -3,3 +3,18 @@ def add(a,b) :
 
 def sub(a,b) :
     return a - b
+
+def maxBlock(word):
+    max_count=0
+    count=1
+    for i in range (len(word)):
+        if i==len(word)-1:
+            break
+
+        if word[i]==word[i+1]:
+            count+=1
+        else:
+            if count>max_count:
+                max_count=count
+            count=1
+    return max_count
