0e54c5a0fac93078fa7f00509465d981f0096f1e
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-18 14:07:41 +0530

    Jun 18  14:07:41

--- LRU/operations.py
+++ LRU/operations.py
@@ -16,7 +16,9 @@ def maxBlock(word):
 
         if word[i]==word[i+1]:
             count = count + 1
+        
         else:
+
             if count > max_count:
                 max_count = count
             count = 1
