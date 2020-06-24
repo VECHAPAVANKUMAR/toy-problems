4da5542fdcfd28e0e987cff49c557189470888fd
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-06-24 17:37:04 +0530

    Jun 24  17:37:04

--- LRU/file10.java
+++ LRU/file10.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class file10 { 
     public static void main(String[] args) 
     { 
-        System.out.println("Main");
+        System.out.println("Main";
     } 
 } 
 
